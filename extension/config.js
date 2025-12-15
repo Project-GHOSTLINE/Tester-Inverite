87ab8e54f711a3d215834fdbb02fad5d336ca44d
Solution Argent Rapide <solutionargentrapide@gmail.com>
2025-12-15 11:47:29 -0500

    FIX FINAL: Tri + Navigation stable

--- extension/config.js
+++ extension/config.js
@@ -2,6 +2,6 @@
 var CONFIG = {
   INVERITE_API_KEY: '09a4b8554857d353fd007d29feca423f446',
   INVERITE_API_URL: 'https://www.inverite.com/api/verifications',
-  RAPPORT_SERVER: 'https://rapportsimple-q2xnz8pb5-project-ghostline.vercel.app',
+  RAPPORT_SERVER: 'https://rapportsimple-etdi0k9ey-project-ghostline.vercel.app',
   VERSION: '9.0.0'
 };
