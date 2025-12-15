13d56212de4006200789a35499f434ad6c7c78ac
Solution Argent Rapide <solutionargentrapide@gmail.com>
2025-12-15 13:06:40 -0500

    Extension v9.0 avec détection prêteurs améliorée

--- extension_v2/config.js
+++ extension_v2/config.js
@@ -2,6 +2,6 @@
 var CONFIG = {
   INVERITE_API_KEY: '09a4b8554857d353fd007d29feca423f446',
   INVERITE_API_URL: 'https://www.inverite.com/api/verifications',
-  RAPPORT_SERVER: 'https://rapportsimple-ii9qsgt2l-project-ghostline.vercel.app',
+  RAPPORT_SERVER: 'https://rapportsimple-f54vegzrb-project-ghostline.vercel.app',
   VERSION: '9.0.0'
 };
