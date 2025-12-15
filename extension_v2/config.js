fddb4995d144106c97c85352b6b42f266ab831c4
Solution Argent Rapide <solutionargentrapide@gmail.com>
2025-12-15 12:13:56 -0500

    Fix: Paie = Crédits uniquement

--- extension_v2/config.js
+++ extension_v2/config.js
@@ -2,6 +2,6 @@
 var CONFIG = {
   INVERITE_API_KEY: '09a4b8554857d353fd007d29feca423f446',
   INVERITE_API_URL: 'https://www.inverite.com/api/verifications',
-  RAPPORT_SERVER: 'https://rapportsimple-ql396i2sk-project-ghostline.vercel.app',
+  RAPPORT_SERVER: 'https://rapportsimple-j2lpyoehc-project-ghostline.vercel.app',
   VERSION: '9.0.0'
 };
