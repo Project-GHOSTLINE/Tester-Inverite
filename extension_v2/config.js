6ab77352b73b1177d3f71b64887bb913c4bc732c
Solution Argent Rapide <solutionargentrapide@gmail.com>
2025-12-15 12:17:19 -0500

    Dashboard avec 8 paies + Revenus autres - v9.0

--- extension_v2/config.js
+++ extension_v2/config.js
@@ -2,6 +2,6 @@
 var CONFIG = {
   INVERITE_API_KEY: '09a4b8554857d353fd007d29feca423f446',
   INVERITE_API_URL: 'https://www.inverite.com/api/verifications',
-  RAPPORT_SERVER: 'https://rapportsimple-j2lpyoehc-project-ghostline.vercel.app',
+  RAPPORT_SERVER: 'https://rapportsimple-egwb1sa2u-project-ghostline.vercel.app',
   VERSION: '9.0.0'
 };
