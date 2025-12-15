cbebc271178ca7ae2e8193c3d252413f75b170b3
Solution Argent Rapide <solutionargentrapide@gmail.com>
2025-12-15 11:08:57 -0500

    FINAL: 2 colonnes + MYDOH retiré

--- extension/config.js
+++ extension/config.js
@@ -2,6 +2,6 @@
 var CONFIG = {
   INVERITE_API_KEY: '09a4b8554857d353fd007d29feca423f446',
   INVERITE_API_URL: 'https://www.inverite.com/api/verifications',
-  RAPPORT_SERVER: 'https://rapportsimple-873yn0qdf-project-ghostline.vercel.app',
+  RAPPORT_SERVER: 'https://rapportsimple-1xt2c8swu-project-ghostline.vercel.app',
   VERSION: '9.0.0'
 };
