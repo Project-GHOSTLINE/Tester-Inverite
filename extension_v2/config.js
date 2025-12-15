4c0db5b33580dc0395b098bc9da4ff24b014f34d
Solution Argent Rapide <solutionargentrapide@gmail.com>
2025-12-15 14:33:50 -0500

    Fix FINAL: extension_v2 avec URL corrigée (n77a0f2ht) - erreur analyse corrigée

--- extension_v2/config.js
+++ extension_v2/config.js
@@ -2,6 +2,6 @@
 var CONFIG = {
   INVERITE_API_KEY: '09a4b8554857d353fd007d29feca423f446',
   INVERITE_API_URL: 'https://www.inverite.com/api/verifications',
-  RAPPORT_SERVER: 'https://rapportsimple-3srta0csx-project-ghostline.vercel.app',
+  RAPPORT_SERVER: 'https://rapportsimple-n77a0f2ht-project-ghostline.vercel.app',
   VERSION: '9.1.3'
 };
